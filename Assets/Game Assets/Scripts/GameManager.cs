f4bb383b670130a7209d40dd3b3bf3abe27c469c
Antoine Brouillette <antoineqc@hotmail.com>
2015-10-09 19:55:50 -0400

    Main Menu + Loading (Base Done)

--- Assets/Game Assets/Scripts/GameManager.cs
+++ Assets/Game Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using DG.Tweening;
 
 public class GameManager : MonoBehaviour {
 
@@ -8,6 +9,7 @@ public class GameManager : MonoBehaviour {
     void Awake() {
         if (instance == null) {
             instance = this;
+            DontDestroyOnLoad(gameObject);
         }
         else {
             Destroy(gameObject);
@@ -28,19 +30,42 @@ public class GameManager : MonoBehaviour {
 
     public enum Scenes {
         Menu,
+        LoadingGame,
         Main
     }
 
     //FUNCTIONS
 
     public void SwitchScene(Scenes scene) {
+        OnSceneEnd(currentScene);
         currentScene = scene;
-        OnSceneStartup(scene);
+        OnSceneStartup(currentScene);
+    }
+
+    void OnSceneEnd(Scenes scene) {
+        switch (scene) {
+            case Scenes.Menu:
+                GameObject.FindWithTag("MainMenuRef").GetComponent<MainMenuRefUI>().mainMenu.DOFade(0, 0.75f);
+                break;
+            case Scenes.LoadingGame:
+                CanvasGroup loading = GameObject.FindWithTag("MainMenuRef").GetComponent<MainMenuRefUI>().loading;
+                loading.DOFade(0, 0.75f);
+                loading.GetComponent<LoadingUI>().StopAllCoroutines();
+                break;
+            case Scenes.Main:
+                break;
+        }
     }
 
     void OnSceneStartup(Scenes scene) {
         switch (scene) {
             case Scenes.Menu:
+                GameObject.FindWithTag("MainMenuRef").GetComponent<MainMenuRefUI>().mainMenu.DOFade(1, 0.75f);
+                break;
+            case Scenes.LoadingGame:
+                CanvasGroup loading = GameObject.FindWithTag("MainMenuRef").GetComponent<MainMenuRefUI>().loading;
+                loading.DOFade(1, 0.75f);
+                loading.GetComponent<LoadingUI>().StartAnim();
                 break;
             case Scenes.Main:
                 break;
