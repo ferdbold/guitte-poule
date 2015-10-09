3f57a7e5ea26249056a1a2a2a1f207848ba5ed65
Antoine Brouillette <antoineqc@hotmail.com>
2015-10-09 18:43:32 -0400

    Scene Push and first step in Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    static public GameManager instance;
+
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnDestroy() {
+        if (instance = this) {
+            instance = null;
+        }
+    }
+
+//-----------------------------------------------------------------------------------------------------------------------
+//////////////////////////////////////////////// SWITCH SCENE ///////////////////////////////////////////////////////////
+    
+    //VARIABLES
+    Scenes currentScene;
+
+    public enum Scenes {
+        Menu,
+        Main
+    }
+
+    //FUNCTIONS
+
+    public void SwitchScene(Scenes scene) {
+        currentScene = scene;
+        OnSceneStartup(scene);
+    }
+
+    void OnSceneStartup(Scenes scene) {
+        switch (scene) {
+            case Scenes.Menu:
+                break;
+            case Scenes.Main:
+                break;
+        }
+    }
+
+//-----------------------------------------------------------------------------------------------------------------------
+////////////////////////////////////////////////              ///////////////////////////////////////////////////////////
+
+}
