ea6866b85e665823ad2f91c36e8083b07c3ee570
Alex <alex3521@hotmail.fr>
2015-10-10 14:49:11 -0400

    Added basic animation for the menu screen

--- Assets/Game Assets/Scripts/UI/MainMenuRefUI.cs
+++ Assets/Game Assets/Scripts/UI/MainMenuRefUI.cs
@@ -1,8 +1,32 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using DG.Tweening;
 
 public class MainMenuRefUI : MonoBehaviour {
 
     public CanvasGroup mainMenu;
     public CanvasGroup loading;
+    public CanvasGroup playButton;
+    public CanvasGroup IntroButton;
+
+    public void ShowPlayButton() {
+        playButton.DOFade(1, 1);
+        playButton.GetComponentInChildren<Button>().interactable = true;
+    }
+
+    public void HidePlayButton() {
+        playButton.DOFade(0, 1);
+        playButton.GetComponentInChildren<Button>().interactable = false;
+    }
+
+    public void ShowIntroButton() {
+        IntroButton.DOFade(1, 1);
+        playButton.GetComponentInChildren<Button>().interactable = true;
+    }
+
+    public void HideIntroButton() {
+        IntroButton.DOFade(0, 1);
+        playButton.GetComponentInChildren<Button>().interactable = false;
+    }
 }
