51fa7ea605c1e73822c9ddcbf6c5e45d1bc62eb7
Frédéric Bolduc <bolducfrederic@gmail.com>
2015-10-10 18:40:02 -0400

    Added basic date event widget

--- Assets/Game Assets/Scripts/UI/Game/DateEventWidget.cs
+++ Assets/Game Assets/Scripts/UI/Game/DateEventWidget.cs
@@ -1,5 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class DateEventWidget : MonoBehaviour {
 
+    [Header("Components")]
+    [SerializeField]
+    private Text questionText;
+    [SerializeField]
+    private Image drawing;
+    [SerializeField]
+    private Button audioPlayButton;
+    [SerializeField]
+    private Text answerText;
 }
