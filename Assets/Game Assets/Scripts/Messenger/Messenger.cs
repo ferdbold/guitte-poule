c6d9867790b9e3233bf03d8866e3b356bc85d651
ironsquirel <sideflip7@hotmail.com>
2015-10-11 15:37:31 -0400

    push distant server ip

--- Assets/Game Assets/Scripts/Messenger/Messenger.cs
+++ Assets/Game Assets/Scripts/Messenger/Messenger.cs
@@ -21,7 +21,7 @@ public class Messenger
         listener;
 
 
-    private string _ip = "192.168.2.165";
+    private string _ip = "54.69.175.11";
     public string ip { get { return this._ip; } set { this._ip = value; } }
 
     private int
